7dea0c28043f393ccc0ae929cd83d794654d075c
haicao <haiit01@icloud.com>
2024-03-17 17:59:17 +0700

    change product and order

--- Assignment_Java6/src/main/java/com/app/rest/controller/OrderRestController.java
+++ Assignment_Java6/src/main/java/com/app/rest/controller/OrderRestController.java
@@ -8,15 +8,19 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.dao.OrderDAO;
 import com.app.entity.Order;
 import com.app.entity.OrderDetail;
+import com.app.entity.Product;
 import com.app.service.OrderService;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -27,6 +31,9 @@ public class OrderRestController {
 	
 	@Autowired
 	OrderService orderService;
+
+	@Autowired
+	OrderDAO dao;
 
 	@PostMapping()
 	public Order create (@RequestBody JsonNode orderData) {
@@ -64,4 +71,8 @@ public class OrderRestController {
 		return orderService.getRevenueForAWeek();
 	}
 	
+	@PutMapping("update")
+	public Order update(@RequestBody Order order) {
+		return dao.save(order);
+	}
 }
